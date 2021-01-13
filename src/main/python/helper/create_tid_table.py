6311fd2fddebda3e020a675edd2319ae44fa947b
niko.popitsch <niko.popitsch@10.0.0.34>
2021-01-13 17:53:42 +0100

    f

--- src/main/python/helper/create_tid_table.py
+++ src/main/python/helper/create_tid_table.py
@@ -16,7 +16,8 @@ if __name__ == '__main__':
     parser.add_argument("-i", "--slamstr_transcript_file", type=str, required=True, dest="slamstr_transcript_file", metavar="slamstr_transcript_file", help="slamstr_transcript_file")
     parser.add_argument("-m", "--min_", type=int, required=True, dest="min_tab_180", help="min value in the TAB_ALL_tpm_180min column")
     parser.add_argument("-o", "--outfile", type=str, required=False, dest="outfile", metavar="outfile", help="output file")
-
+    args = parser.parse_args()
+    
     m=args.min_tab_180
     d=pd.read_csv(args.slamstr_transcript_file,delimiter='\t',encoding='utf-8')
     d=d[d['TAB_ALL_tpm_180min']>=m]

05d6c3802fc44c82602cf2b4b31693b380265dcd
niko.popitsch <niko.popitsch@10.0.0.34>
2021-01-14 17:18:37 +0100

    f

--- src/main/python/splice_sim/model.py
+++ src/main/python/splice_sim/model.py
@@ -241,7 +241,7 @@ class Model():
         # load + filter gene gff
         logging.info("Loading gene GFF")
         self.gff = pr.read_gff3(config["gene_gff"])
-        self.gff = self.gff[gff.transcript_id.isin(list(config['transcripts'].keys()))] # reduce to contain only configured transcripts
+        self.gff = self.gff[self.gff.transcript_id.isin(list(config['transcripts'].keys()))] # reduce to contain only configured transcripts
 
         # load genome
         logging.info("Loading genome")

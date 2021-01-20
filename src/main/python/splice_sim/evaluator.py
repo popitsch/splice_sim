e8f2372ba4ae7fdc4cf0cbaec02ce8a872958b6b
niko.popitsch <niko.popitsch@10.0.0.34>
2021-01-20 10:34:57 +0100

    f

--- src/main/python/splice_sim/evaluator.py
+++ src/main/python/splice_sim/evaluator.py
@@ -65,16 +65,16 @@ def classify_read(read, overlapping_tids, is_converted, mapper, condition, perfo
     
     # read strongly overlaps with real location; FIXME: check strand!
     if overlap >= overlap_cutoff:
-        performance[true_tid, 'TP'] += 1
+        performance[true_tid, true_isoform, 'TP'] += 1
     else:
-        performance[true_tid, 'FN'] += 1
+        performance[true_tid, true_isoform, 'FN'] += 1
         print('\t'.join([str(x) for x in (read_coord, true_coord, 'FN', 'NA', 
                                           1 if is_converted else 0, mapper, condition, overlap, true_tid, 
                                           true_strand, true_isoform, tag, true_chr,n_true_seqerr, n_tc_pos, 
                                           ','.join(overlapping_tids) if len(overlapping_tids) > 0 else 'NA', 
                                           read_name)]), file=out_reads)
         for tid in overlapping_tids:
-            performance[tid, 'FP'] += 1/len(overlapping_tids)
+            performance[tid, true_isoform, 'FP'] += 1/len(overlapping_tids)
             print('\t'.join([str(x) for x in (read_coord, true_coord, 'FP', tid, 
                                               1 if is_converted else 0, mapper, condition, overlap, true_tid, 
                                               true_strand, true_isoform, tag, true_chr,n_true_seqerr, n_tc_pos, 
@@ -93,7 +93,7 @@ def evaluate_bam(bam_file, is_converted, m, mapper, condition, out_reads, out_pe
     n_reads=0
     for c, c_len in dict_chr2len.items():
         df = m.df[(m.df.Feature == 'transcript') & (m.df.Chromosome == c)]  # get annotations
-        print("Processing chromosome %s of %s/%s/%s"  % (c,  is_converted, mapper, condition) )
+        #print("Processing chromosome %s of %s/%s/%s"  % (c,  is_converted, mapper, condition) )
         if df.empty: # no trasncript on this chrom: all reads are FN
             rit = ReadIterator(bam_file, dict_chr2idx, reference=c, start=1, end=c_len, max_span=None, flag_filter=0) # max_span=m.max_ilen
             for loc, read in rit:
@@ -106,9 +106,12 @@ def evaluate_bam(bam_file, is_converted, m, mapper, condition, out_reads, out_pe
             for loc, (read, annos) in it:
                 n_reads+=1
                 overlapping_tids = [t[0] for (_, (_, t)) in annos[0]]
-                performance = classify_read(read, overlapping_tids, is_converted, mapper, condition, performance, out_reads)       
-    for tid in set([x for x, y in performance.keys()]):
-         print("%s\t%s\t%s\t%s\t%i\t%i\t%i" % (1 if is_converted else 0, mapper, condition, tid, performance[tid, 'TP'], performance[tid, 'FP'], performance[tid, 'FN']), file=out_performance) 
+                performance = classify_read(read, overlapping_tids, is_converted, mapper, condition, performance, out_reads)
+    tids= set([x for x, y, z in performance.keys()])
+    isos= set([y for x, y, z in performance.keys()])
+    for tid in tids:
+        for iso in isos:
+             print("%s\t%s\t%s\t%s\t%i\t%i\t%i" % (1 if is_converted else 0, mapper, condition, iso, tid, performance[tid, 'TP'], performance[tid, 'FP'], performance[tid, 'FN']), file=out_performance) 
     print("reads:  %i " % n_reads)
 # #bam='/Volumes/groups/ameres/Niko/projects/Ameres/splicing/splice_sim/testruns/small4/small4/sim/bam_tc/STAR/small4.5min.STAR.TC.bam'
 # bam='/Volumes/groups/ameres/Niko/projects/Ameres/splicing/splice_sim/testruns/small4/small4/sim/bam_tc/HISAT2_TLA/small4.5min.HISAT2_TLA.TC.bam'
@@ -149,7 +152,7 @@ def evaluate_dataset(config, config_dir, simdir, outdir, overwrite=False):
     with open(fout, 'w') as out:
         with open(fout2, 'w') as out2:
             print("mapped_coords\ttrue_coords\tclassification\ttid\tis_converted\tmapper\tcondition\toverlap\ttrue_tid\ttrue_strand\ttrue_isoform\ttag\ttrue_chr\tn_true_seqerr\tn_tc_pos\toverlapping_tids\tread_name", file=out)
-            print("is_converted\tmapper\tcondition\ttid\tTP\tFP\tFN", file=out2)
+            print("is_converted\tmapper\tcondition\tiso\ttid\tTP\tFP\tFN", file=out2)
             for cond in m.conditions:
                 for mapper in config['mappers'].keys():
                     final_ori = simdir + "bam_ori/" + mapper + "/" + config['dataset_name'] + "." + cond.id + "." + mapper + ".bam"

29f21a393b6e58691d84f3234fca88735e674e05
niko.popitsch <niko.popitsch@eduroam-016-033.wlan.univie.ac.at>
2021-11-15 14:23:58 +0100

    tested

--- src/main/python/splice_sim/evaluator2.py
+++ src/main/python/splice_sim/evaluator2.py
@@ -1222,14 +1222,14 @@ def evaluate_dataset(config, config_dir, simdir, outdir, overwrite=False):
                                                 mappability_tid_conv_out = bam_out_dir_conv + config['dataset_name'] + "." + cond.id + "." + mapper + ".conv.tid_mappability.bedGraph"
                                                 mappability_SJ_conv_out = bam_out_dir_conv + config['dataset_name'] + "." + cond.id + "." + mapper + ".conv.SJ_mappability.bedGraph"
         
-                                                ##evaluate_bam(bam_ori, bam_ori_out, False, m, mapper, cond, out, out2)
-                                                ##evaluate_splice_sites(bam_ori, bam_ori_out_intron, False, m, mapper, cond.id, out3)
+                                                evaluate_bam(bam_ori, bam_ori_out, False, m, mapper, cond, out, out2)
+                                                evaluate_splice_sites(bam_ori, bam_ori_out_intron, False, m, mapper, cond.id, out3)
                                                 # evaluate_coverage_uniformity(bam_ori, bam_ori_truth, False, m, mapper, cond.id, out4, out5, out6)
                                                 calculate_splice_site_mappability(config, bam_ori, bam_ori_truth, False, m, mapper, cond.id, out7, mappability_SJ_ori_out, config["genome_fa"], config["readlen"])
                                                 get_mapq_histogram(bam_ori, False, m, mapper, cond, out9)
                                                 
-                                                ##evaluate_bam(bam_conv, bam_conv_out, True, m, mapper, cond, out, out2)
-                                                ##evaluate_splice_sites(bam_conv, bam_conv_out_intron, True, m, mapper, cond.id, out3)
+                                                evaluate_bam(bam_conv, bam_conv_out, True, m, mapper, cond, out, out2)
+                                                evaluate_splice_sites(bam_conv, bam_conv_out_intron, True, m, mapper, cond.id, out3)
                                                 # evaluate_coverage_uniformity(bam_conv, bam_conv_truth, True, m, mapper, cond.id, out4, out5, out6)
                                                 calculate_splice_site_mappability(config, bam_conv, bam_conv_truth, True, m, mapper, cond.id, out7, mappability_SJ_conv_out, config["genome_fa"], config["readlen"])
                                                 get_mapq_histogram(bam_conv, True, m, mapper, cond, out9)
